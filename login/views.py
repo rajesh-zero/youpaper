d9a807e1942991343d1cb79bc77a0ec81ff22678
rajesh-zero <rajesh_zero@outlook.com>
2019-09-30 23:33:08 +0530

    added register and learnt to link back directory check line 7 in register html

--- login/views.py
+++ login/views.py
@@ -6,14 +6,18 @@ from django.http import HttpResponse
 #     return HttpResponse("login page")
 def login(request):
     return render(request,'login/login.html')
+
+
 def register(request):
-    return render(request,'login/login.html')
+    return render(request,'login/register.html')
+
+
 def loguserin(request):
-    
+
     emailid = request.POST['email']     #This is valid
     #emailid = request.POST.get('email','mera mail')    #this is also valid
     password = request.POST['password']
     rememberme = request.POST.get('remember','off') # returns 0n or off for checkbox by default
-    print(emailid," ",password," ",rememberme)
+    #print(emailid," ",password," ",rememberme)
 
     return render(request,'login/test.html',{'email':emailid,'password':password})
