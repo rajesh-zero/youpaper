b967ca9f031038f2f40c26942ba6cd30d75c919c
rajesh-zero <rajesh_zero@outlook.com>
2019-10-02 00:25:41 +0530

    updated timezone

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import users # imported users table from model
 # Register your models here.
+
+admin.site.register(users) #registered my table user to admin
