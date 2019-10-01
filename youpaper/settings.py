250c960c9c0390834be4c4027176f0c16b980bf4
rajesh-zero <rajesh_zero@outlook.com>
2019-10-01 23:25:17 +0530

    created database connection

--- youpaper/settings.py
+++ youpaper/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
+    'login', #added login app here
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'youpaper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'youpaper',
+        'USER':'postgres',
+        'PASSWORD':'admin123',
+        'HOST':'localhost'
     }
 }
 
