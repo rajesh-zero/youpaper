6437dda34ef9635b1f122b0ea73b56974788583c
rajesh-zero <rajesh_zero@outlook.com>
2019-10-02 00:19:13 +0530

    created superuser

--- login/models.py
+++ login/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 # to check sql query it will fire run command python manage.py sqlmigrate login 0001 and to commit that run python manage.py migrate
-
+#create super user by running python manage.py createsuperuser ( I created username = admin, email = admin@email.com , password = admin123)
 class users(models.Model):
     user_id = models.AutoField(primary_key=True)
     user_email = models.CharField(max_length=100)
