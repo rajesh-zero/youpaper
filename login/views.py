43707f59019b5034e7a3f0cec2111394943b4037
rajesh-zero <rajesh_zero@outlook.com>
2019-10-02 16:13:51 +0530

    calling template from another app

--- login/views.py
+++ login/views.py
@@ -18,6 +18,8 @@ def loguserin(request):
     #emailid = request.POST.get('email','mera mail')    #this is also valid
     password = request.POST['password']
     rememberme = request.POST.get('remember','off') # returns 0n or off for checkbox by default
+    if rememberme == 'on':
+        pass
     #print(emailid," ",password," ",rememberme)
 
-    return render(request,'login/test.html',{'email':emailid,'password':password})
+    return render(request,'userprofile/profile.html',{'email':emailid,'password':password}) # calling profile page of userprofile application
