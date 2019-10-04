4f642ece9216e627cf611323a5ea9ccb7de08e6d
rajesh-zero <rajesh_zero@outlook.com>
2019-10-04 13:27:33 +0530

    added comments to disable pylint errors and warnings

--- login/models.py
+++ login/models.py
@@ -7,19 +7,19 @@ from django.db import models
 # login 0001 and to commit that run python manage.py migrate
 
 #create super user by running python manage.py createsuperuser
-# ( I created username = admin, email = admin@email.com , password = admin123)
-
+#below line added to remove pylint error
+#pylint:disable=invalid-name
 class users(models.Model):
     '''doc string'''
     user_id = models.AutoField(primary_key=True)
     user_email = models.CharField(max_length=100)
     user_password = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
-    user_gender = models.CharField(max_length=1,default="")
+    user_gender = models.CharField(max_length=1, default="")
     user_mobile = models.CharField(max_length=70, default="")
-    user_image = models.ImageField(upload_to="login/userimage",default="")
+    user_image = models.ImageField(upload_to="login/userimage", default="")
     user_dob = models.DateField(null=True)
-    user_description = models.CharField(max_length=300,default="")
+    user_description = models.CharField(max_length=300, default="")
 
     def __str__(self): # so that it displays email in admin objects
         return self.user_email
--- login/views.py
+++ login/views.py
@@ -24,11 +24,8 @@ def loguserin(request):
     '''
     stupid thing to disable pylint warning
     '''
-    emailid = request.POST.get('email')  
-    password = request.POST.get('password')   
-    #rememberme = request.POST.get('remember','off') # returns 0n or off for checkbox by default
-    # if rememberme == 'on':
-    #     pass
+    emailid = request.POST.get('email')
+    password = request.POST.get('password')
     user_data = users.objects.get(user_email=emailid) 
     #here was an error for that install pip install pylint-django
     if user_data.user_password == password:
@@ -37,24 +34,21 @@ def loguserin(request):
     # calling profile page of userprofile application
 
 # def registered(request):
-
     # emailid = request.POST.get('email','mera mail')    #this is also valid
     # password = request.POST.get('password','mera mail')   
     # password = request.POST.get('name')
-
-
-
     # return render(request,'login/register.html',{'register':"registered"})
 def registered(request):
     '''
     stupid thing to disable pylint warning
     '''
     if request.method == 'POST':
-        emailid = request.POST.get('email')    
-        name = request.POST.get('name')   
-        password = request.POST.get('password')   
-        user = users(user_email=emailid, user_password=password, user_name=name)
-        user.save()
-        return HttpResponse("<script>alert('registered successfully')</script>")
+        emailid = request.POST.get('email')
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        if emailid != '' and password != '':
+            user = users(user_email=emailid, user_password=password, user_name=name)
+            user.save()
+            return HttpResponse("<script>alert('registered successfully')</script>")
     return HttpResponse("<script>alert('something went wrong')</script>")
     
