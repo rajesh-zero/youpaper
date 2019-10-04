1b4e3a5870bda67a41b25bd761337f37e5d1c212
rajesh-zero <rajesh_zero@outlook.com>
2019-10-04 12:18:52 +0530

    all pylint django errors removed

--- login/views.py
+++ login/views.py
@@ -1,3 +1,4 @@
+'''views.py for login '''
 from django.shortcuts import render
 from django.http import HttpResponse
 from login.models import users
@@ -6,44 +7,54 @@ from login.models import users
 # def login(request):
 #     return HttpResponse("login page")
 def login(request):
-    return render(request,'login/login.html')
+    '''
+    stupid thing to disable pylint warning
+    '''
+    return render(request, 'login/login.html')
 
 
 def register(request):
-    return render(request,'login/register.html')
+    '''
+    stupid thing to disable pylint warning
+    '''
+    return render(request, 'login/register.html')
 
 
 def loguserin(request):
-
+    '''
+    stupid thing to disable pylint warning
+    '''
     emailid = request.POST.get('email')  
     password = request.POST.get('password')   
     #rememberme = request.POST.get('remember','off') # returns 0n or off for checkbox by default
     # if rememberme == 'on':
     #     pass
-    u = users.objects.get(user_email=emailid) #here was an error for that install pip install pylint-django
-    if u.user_password == password:
+    user_data = users.objects.get(user_email=emailid) 
+    #here was an error for that install pip install pylint-django
+    if user_data.user_password == password:
         return HttpResponse("login successful")
-
-
-    return render(request,'userprofile/profile.html',{'email':emailid,'password':password}) # calling profile page of userprofile application
+    return render(request, 'userprofile/profile.html', {'email': emailid, 'password':password}) 
+    # calling profile page of userprofile application
 
 # def registered(request):
 
     # emailid = request.POST.get('email','mera mail')    #this is also valid
     # password = request.POST.get('password','mera mail')   
-    # password = request.POST.get('name')   
+    # password = request.POST.get('name')
 
 
 
     # return render(request,'login/register.html',{'register':"registered"})
 def registered(request):
-
+    '''
+    stupid thing to disable pylint warning
+    '''
     if request.method == 'POST':
         emailid = request.POST.get('email')    
         name = request.POST.get('name')   
         password = request.POST.get('password')   
-        user = users(user_email=emailid,user_password=password,user_name=name)
+        user = users(user_email=emailid, user_password=password, user_name=name)
         user.save()
         return HttpResponse("<script>alert('registered successfully')</script>")
-    else:
-        return HttpResponse("<script>alert('something went wrong')</script>")
+    return HttpResponse("<script>alert('something went wrong')</script>")
+    
