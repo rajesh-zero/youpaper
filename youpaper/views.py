4a846db004139b596734f1bfddadef92f5acb2a3
rajesh-zero <rajesh_zero@outlook.com>
2019-10-07 00:35:22 +0530

    data fetch on homepage and page is responsive

--- youpaper/views.py
+++ youpaper/views.py
@@ -1,11 +1,15 @@
 """doc string"""
 from django.shortcuts import render
 from django.http import HttpResponse
+from ypdb.models import Ypdb
 # Create your views here.
 
 def home(request):
     """doc string"""
-    return render(request, 'home.html',)
+    datas = Ypdb.objects.filter(ypdb_poster__isnull=False)
+    print(datas)
+    params = {'datas':datas, 'range':range(6)}
+    return render(request, 'home.html', params)
 def contactus(request):
     '''doc string'''
     return HttpResponse("contact us")
