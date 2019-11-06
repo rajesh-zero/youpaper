050c7c71588c6122fe0dc4bab73a52ff07cfc155
rajesh-zero <rajesh_zero@outlook.com>
2019-11-06 21:39:08 +0530

    pagination working on homepage

--- youpaper/views.py
+++ youpaper/views.py
@@ -2,8 +2,9 @@
 import json
 import requests
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator#for pagination
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q #to use ~Q to do negation
 from ypdb.models import Ypdb
 from codes.apimethods import search_api
 # Create your views here.
@@ -19,9 +20,14 @@ def home(request):
     #     print('if ke andhar hai')
     # else:
     #     request.session['user_email'] = ''
-    datas = Ypdb.objects.filter(~Q(ypdb_poster='N/A')).order_by('-ypdb_id')[:20]#to get only last 20records with images in descending order
+    datas = Ypdb.objects.filter(~Q(ypdb_poster='N/A')).order_by('-ypdb_id')[:100]#to get only last 20records with images in descending order
     #print(datas)
-    params = {'datas':datas, 'range':range(6)}
+    paginator = Paginator(datas, 12) # Show 25 contacts per page
+    page = request.GET.get('page')
+    data = paginator.get_page(page)
+
+    params = {'datas':data, 'range':range(6)}
+    print(params['datas'])
     return render(request, 'home.html', params)
 def contactus(request):
     '''doc string'''
